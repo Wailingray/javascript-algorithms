eae22da4867c627ef638f2eaa4ea1d459f92ea58
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2021-07-05 16:19:45 +0300

    4th task is done

--- 04-capitalize/script.js
+++ 04-capitalize/script.js
@@ -7,9 +7,19 @@
 */
 
 function capitalize(str) {
-    // Напишите код здесь
+    strArray = str.split(' ');
+    let newStr = '';
+
+    strArray.forEach(function (el) {
+        let firstLetter = el.slice(0,1).toUpperCase()
+        let remains = el.slice(-(el.length - 1)).toLowerCase();
+        newStr += (firstLetter + remains + ' ');
+    });
+
+    return newStr;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(capitalize('молодость всё простит')); // "Молодость Всё Простит"
+console.log(capitalize('молодость всё простит'));
+console.log(capitalize('Испробовать их Все Должны Вы')); // "Молодость Всё Простит"
