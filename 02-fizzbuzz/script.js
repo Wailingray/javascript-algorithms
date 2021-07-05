76855d413c595fd514efcd83844a9f3b34abadb1
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2021-07-05 11:32:25 +0300

    solved 2nd task

--- 02-fizzbuzz/script.js
+++ 02-fizzbuzz/script.js
@@ -11,9 +11,17 @@
 */
 
 function fizzBuzz(num) {
-    // Напишите код здесь
+    for (let i = 1; i <= num; i++) {
+        let stepWord = '';
+
+        if (i % 3 === 0) stepWord += 'fizz';
+        if (i % 5 === 0) stepWord += 'buzz';
+        
+        if (stepWord === '') console.log(i);
+        else console.log(stepWord);
+    }
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(fizzBuzz(15));
+console.log(fizzBuzz(99));
