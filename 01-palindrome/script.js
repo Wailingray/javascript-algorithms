58b9ab4c0119cad4901573694dea0c769824118b
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2021-07-05 10:41:32 +0300

    solved 1st task

--- 01-palindrome/script.js
+++ 01-palindrome/script.js
@@ -13,20 +13,37 @@
  * 
 */
 
-function palindrome(str) {
-    // Напишите код здесь
+/*Функция удаления знаков препинания и пробелов*/
+function noPunct(str) {
+    const punctRE = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g; // почти вся возможная пунктуация
+    const spaceRE = /\s+/g; // пробелы
+    str = str.replace(punctRE, '').replace(spaceRE, '');
+    return str;
 }
+
+
+function palindrome(str) {
+    str = noPunct(str); //очищаем входные данные от пробелов и пунктуации
+    let reversedString = '';
+    for (let i = 0; i < str.length; i += 1) {
+        reversedString += str[str.length - (1 + i)];
+      }
+    if (reversedString.toLowerCase() === str.toLowerCase()) return true;
+    else return false;
+}
+
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
 console.log(palindrome('топот')); // должно быть true
 console.log(palindrome('Saippuakivikauppias')); // true
 console.log(palindrome('привет')); // false
+console.log(palindrome('О, лета тело!'));
 
 /*
  * Бонус. Задача для любознательных. Пусть функция принимает на вход любую строку,
  * но пробелы и знаки препинания не учитывает. Например:
  * 
- * palindrome('О, лета тело!'); // true
+ // true
  * 
 */
