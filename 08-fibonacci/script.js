07f0f2bc9af9411ffc626e36f24adf271ed69e99
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2021-07-06 14:22:07 +0300

    Update script.js

--- 08-fibonacci/script.js
+++ 08-fibonacci/script.js
@@ -10,13 +10,17 @@
 
 function fibonacci(n) {
     const simplMass = Array.from({ length: n }, (v, i) =>  i); 
+    //simplMass.unshift(0)
+    console.log(simplMass);
     let fibMass = [];
     simplMass.reduce(function(prevVal, el){
-        fibMass.push(prevVal + el);
+        fibMass.push(el);
+        fibMass.push(el + prevVal);
+        return el   
     }, 0);
     console.log(fibMass);
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(fibonacci(4)); // 2. Четвёртое число последовательности — двойка (0, 1, 1, 2)
+console.log(fibonacci(7)); // 2. Четвёртое число последовательности — двойка (0, 1, 1, 2)
