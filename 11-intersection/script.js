eba3e30769f189620f41e9a73cacd5d78dc86c12
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2021-07-06 19:42:32 +0300

    solved №11

--- 11-intersection/script.js
+++ 11-intersection/script.js
@@ -7,11 +7,35 @@
  * 
 */
 
-function intersection(arr1, arr2) {
-    // Напишите код здесь
+/*Функция из задания 6 для удаления дубликатов*/
+function uniq(arr) {
+    const uniqueArr = arr.filter(function (item, position, array) {
+        return array.indexOf(item) === position; 
+        });
+    return uniqueArr;
 }
+
+
+function intersection(arr1, arr2) {
+    let resultArr = [];
+    if(arr1.length >= arr2.length) {
+        for(let i = 0; i < arr2.length; i++) {
+            if (arr1.includes(arr2[i])) resultArr.push(arr1[i])
+        }
+    }
+    else {
+        for(let i = 0; i < arr1.length; i++) {
+            if (arr2.includes(arr1[i])) resultArr.push(arr1[i])
+        }
+    }
+     return uniq(resultArr);
+    }
+    
+
+    
+
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(intersection([1, 5, 4, 2], [8, 91, 4, 1, 3])); // [4, 1]
-console.log(intersection([1, 5, 4, 2], [7, 12])); // []
+console.log(intersection([1, 1, 5, 4, 2], [8, 91, 4, 1, 1, 3])); // [4, 1]
+//console.log(intersection([1, 5, 4, 2], [7, 12])); // []
