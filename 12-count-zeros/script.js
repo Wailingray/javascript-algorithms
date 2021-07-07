1f62c83480e35005f1c5d00b75bd91f122d7a449
Ivan Sergeev <82928893+Wailingray@users.noreply.github.com>
2021-07-07 10:51:42 +0300

    Update script.js

--- 12-count-zeros/script.js
+++ 12-count-zeros/script.js
@@ -7,10 +7,20 @@
 */
 
 function countZeros(n) {
-    // Напишите код здесь
+    let counter = 0;
+    for(let i = 1; i <= n; i++) {
+        if (i % 10 === 0) {
+            let a = i / 10;
+            console.log(a)
+            counter += a.toString().length;
+            //console.log(counter)
+        }
+    }       
+    return counter;
 }
+
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(countZeros(20)); // 2 – два нуля, по одному в числах 10 и 20
-console.log(countZeros(100)); // 11 – 11 нулей в числах: 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+//console.log(countZeros(20)); // 2 – два нуля, по одному в числах 10 и 20
+console.log(countZeros(342)); // 11 – 11 нулей в числах: 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
